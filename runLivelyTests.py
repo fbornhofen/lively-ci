73fb551437742388a395829cbf2c21219578db4a
Fabian Bornhofen <fbornhofen@googlemail.com>
2011-12-12 16:39:51 -0800

    display stack trace for exceptions

--- runLivelyTests.py
+++ runLivelyTests.py
@@ -70,7 +70,10 @@ def reportResults(results):
   #print json.dumps(results['failed'], indent=4, sort_keys=True)
   for failed in results['failed']:
     print failed['classname'] + ">>" + failed['selector']
-    print failed['err']['message']
+    if failed.has_key('message'):
+      print failed['err']['message']
+    elif failed.has_key('stack'):
+      print failed['err']['stack']
     print ''
   if (results['testsFailed'] > 0):
     return -1
